5004881f67a0aa915d5f742cb5fcb7cf5c8687ec
LICHKING <savitarreus@gmail.com>
2021-11-11 00:22:52 +0200

    ❄️ Field refactored ❄️

--- game.py
+++ game.py
@@ -3,22 +3,32 @@ import time
 import os
 
 
-def draw():
-	field = [
-		['╔', '═', '═', '═', '═', '═', '═', '═', '╗'],
-		['║', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '║'],
-		['║', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '║'],
-		['║', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '║'],
-		['║', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '║'],
-		['║', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '║'],
-		['║', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '║'],
-		['║', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '║'],
-		['║', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '║'],
-		['║', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '║'],
-		['║', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '║'],
-		['╚', '═', '═', '═', '═', '═', '═', '═', '╝'],
-		]
-	return field
+def draw(w, h):
+	top = ['╔', '╗']
+	bot = ['╚', '╝']
+	middle = ['║', '║']
+	field = []
+
+	for _ in range(w):
+		top.insert(1, '═')
+		bot.insert(1, '═')
+		middle.insert(1, ' ')
+	
+	for _ in range(h):
+		field.append(middle)
+
+	field.append(bot)
+	field.insert(0, top)
+
+	result = ''
+
+	for y in field:
+		temp = ''
+		for x in y:
+			temp += str(x)
+		result += temp + '\n'
+
+	return result
 
 
 def press_instruction(key):
@@ -33,3 +43,5 @@ keyboard.Listener(
 	on_press=press_instruction,
 	on_release=release_instruction
 ).start()
+
+print(draw(30, 10))
